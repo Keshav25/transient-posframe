6d6108b700b2669351293e8ac1c93255c2e1923a
Jonas Bernoulli <jonas@bernoul.li>
2024-11-11 15:07:53 +0100

    transient-posframe--show-buffer: Do not select posframe
    Transient makes sure that the same window, which was selected before
    the menu window was created, remains selected.  If we select the
    posframe here, that's just something Transient has to undo.

--- transient-posframe.el
+++ transient-posframe.el
@@ -84,20 +84,20 @@ When 0, no border is showed."
 (defun transient-posframe--show-buffer (buffer _alist)
   "Show BUFFER in posframe and we do not use _ALIST at this period."
   (when (posframe-workable-p)
-    (let* ((posframe
-	    (posframe-show buffer
-			   :font transient-posframe-font
-			   :position (point)
-			   :poshandler transient-posframe-poshandler
-			   :background-color (face-attribute 'transient-posframe :background nil t)
-			   :foreground-color (face-attribute 'transient-posframe :foreground nil t)
-			   :initialize #'transient-posframe--initialize
-			   :min-width transient-posframe-min-width
-			   :min-height transient-posframe-min-height
-			   :internal-border-width transient-posframe-border-width
-			   :internal-border-color (face-attribute 'transient-posframe-border :background nil t)
-			   :override-parameters transient-posframe-parameters)))
-      (frame-selected-window posframe))))
+    (posframe-show
+     buffer
+     :font transient-posframe-font
+     :position (point)
+     :poshandler transient-posframe-poshandler
+     :background-color (face-attribute 'transient-posframe :background nil t)
+     :foreground-color (face-attribute 'transient-posframe :foreground nil t)
+     :initialize #'transient-posframe--initialize
+     :min-width transient-posframe-min-width
+     :min-height transient-posframe-min-height
+     :internal-border-width transient-posframe-border-width
+     :internal-border-color (face-attribute 'transient-posframe-border
+					    :background nil t)
+     :override-parameters transient-posframe-parameters)))
 
 (defun transient-posframe--initialize ()
   "Initialize transient posframe."
